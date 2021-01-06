9a54a3227db6f1c736c5e73004715a29cb03918e
PaulinaJur <pa.jur7@gmail.com>
2021-01-06 19:02:42 +0100

    modyfing products_filters.py

--- Tests/products_filters.py
+++ Tests/products_filters.py
@@ -13,6 +13,25 @@ class ProductFiltersTests(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
+    def test_laptop_price_and_manuf_filters(self):
+        base_url = 'https://www.x-kom.pl/'
+        driver = self.driver
+        driver.get(base_url)
+        laptop_button_xpath = '//*[@id="app-TopBar"]/div/header/div[2]/div/div/div/nav/ul/li[1]/a'
+        laptop_subcategory_xpath = '//span[@title="Laptopy/Notebooki/Ultrabooki"]'
+        lenovo_filters_xpath = '//*[@id="listing-filters"]/div[2]/div/section[1]/div[1]/div/label/span[2]/span/span[1]'
+        # 1st subtest: laptopy i komputery link
+        check_link(self, driver, 'laptop link', 'https://www.x-kom.pl/', laptop_button_xpath,
+                               'g/2-laptopy-i-komputery.html', None)
 
-    def test_laptop_price_and_manufactor_filters(self):
-        pass
+        # 2nd subtest: laptopu/notebooi/ultrabooki subcategory link
+        check_link(self, driver, 'laptop subcategory link', driver.current_url, laptop_subcategory_xpath,
+                   None, 'https://www.x-kom.pl/g-2/c/159-laptopy-notebooki-ultrabooki.html', 0)
+
+        # 3rd subtest: choosing lenovo filter
+        check_link(self, driver, 'lenovo filter', driver.current_url, lenovo_filters_xpath,
+                   None, 'https://www.x-kom.pl/g-2/c/159-laptopy-notebooki-ultrabooki.html?f[manufacturers][46]=1')
+
+        # 4th subtest: lenovo filter - checking results
+
+
